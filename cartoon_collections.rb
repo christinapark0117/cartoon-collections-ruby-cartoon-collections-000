3ca989d6c6d340cf12333a06c596707829fd75ef
Christina Park <christinapark0117@gmail.com>
2019-09-04 23:09:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,7 @@
 def roll_call_dwarves(dwarves)
+  dwarves.delete_if.with_index(1) do |dwarves, index|
+    index > dwarves.length/2
+  end
   dwarves.each.with_index(1) do |dwarves, index|
     puts "#{index}. #{dwarves}"
  end
@@ -10,17 +13,17 @@ def summon_captain_planet(planeteer_call)
 end
 
 def long_planeteer_calls(calls)
-  call = 0
-  if calls.any? {|call| call.length > 4}
-    return true
-  else
-    return false
- end
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(strings)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camembert", "swiss"]
   strings.find do |cheese|
     cheese_types.include? (cheese)
  end
 end
+
+def words_with_b(words)
+  words.select do |word|
+    word.start_with? ("b")
+  end
